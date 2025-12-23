ba1afe380037318ea973f94156a02124ac39fe7c
ZaYn Miraj <70719806+Zaynmiraj@users.noreply.github.com>
2025-12-23 11:48:08 +0600

    v1.1.4: Fix iOS - add setTintColorString method declaration to header

--- ios/NimGlassBlurView.h
+++ ios/NimGlassBlurView.h
@@ -11,4 +11,9 @@
 @property(nonatomic, assign) CGFloat blurRadius;
 @property(nonatomic, strong) UIColor *tintColor;
 
+/**
+ * Set tint color from string (rgba, rgb, or hex format)
+ */
+- (void)setTintColorString:(NSString *)colorString;
+
 @end
